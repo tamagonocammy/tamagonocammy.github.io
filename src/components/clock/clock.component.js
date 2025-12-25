5ad7df03cd163449f675dfa56df46b811dec70d9
Millie Valencia <jcvc2000@gmail.com>
2025-12-24 21:34:14 -0500

    Updated clock function

--- src/components/clock/clock.component.js
+++ src/components/clock/clock.component.js
@@ -6,6 +6,7 @@ class Clock extends Component {
 
   constructor() {
     super();
+    this.isExtended = false;
   }
 
   imports() {
@@ -19,11 +20,23 @@ class Clock extends Component {
             font: 300 9pt 'Roboto', sans-serif;
             color: ${CONFIG.palette.text};
             letter-spacing: .5px;
+            cursor: pointer;
+            transition: color 0.2s ease;
+        }
+
+        .clock-time:hover {
+            color: ${CONFIG.palette.maroon};
         }
 
         .clock-icon {
             font-size: 10pt;
             margin-right: 10px;
+            cursor: pointer;
+            transition: color 0.2s ease;
+        }
+
+        .clock-icon:hover {
+            color: ${CONFIG.palette.text};
         }
     `;
   }
@@ -41,17 +54,38 @@ class Clock extends Component {
 
   setTime() {
     const date = new Date();
-    this.refs.clock = date.toLocaleString("es-CO", {
-      hour: "numeric",
-      minute: "numeric",
-      hour12: true
-    });
+    if (this.isExtended) {
+      this.refs.clock = date.toLocaleString("es-CO", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      });
+    } else {
+      this.refs.clock = date.toLocaleString("es-CO", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      });
+    }
+  }
+
+  toggleView() {
+    this.isExtended = !this.isExtended;
+    this.setTime();
   }
 
   connectedCallback() {
     this.render().then(() => {
       this.setTime();
       this.setIconColor();
+
+      // Add click event listener to toggle view
+      this.refs.clock.addEventListener("click", () => this.toggleView());
+      this.refs.icon.addEventListener("click", () => this.toggleView());
 
       setInterval(() => this.setTime(), 1000);
     });
