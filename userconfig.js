792e294a7dad088a889a1fece21e2ae2d97019bc
Millie Valencia <jcvc2000@gmail.com>
2026-04-11 19:54:01 -0500

    Update tab background images

--- userconfig.js
+++ userconfig.js
@@ -89,7 +89,7 @@ const default_config = {
     {
       name: "M i l i e",
       color: palette.lavender,
-      background_url: "src/img/banners/banner_14.gif",
+      background_url: "src/img/banners/banner_13.gif",
       categories: [
         {
           name: "principal",
@@ -277,7 +277,7 @@ const default_config = {
     {
       name: "c h i l l",
       color: palette.rosewater,
-      background_url: "src/img/banners/cbg-2.gif",
+      background_url: "src/img/banners/banner_08.gif",
       categories: [
         {
           name: "redes sociales",
