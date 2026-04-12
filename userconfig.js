2fa498725e8151e456d4036545e7cb00138f14ea
Millie Valencia <jcvc2000@gmail.com>
2026-04-12 09:41:25 -0500

    Add banner images and set background to banner_21

--- userconfig.js
+++ userconfig.js
@@ -89,7 +89,7 @@ const default_config = {
     {
       name: "M i l i e",
       color: palette.lavender,
-      background_url: "src/img/banners/banner_13.gif",
+      background_url: "src/img/banners/banner_21.gif",
       categories: [
         {
           name: "principal",
